0519b33bc5785cb9d962e9ef45e6382587b334d6
Sanjeet Shrestha <sanjeetshrestha5@gmail.com>
2024-06-19 16:35:47 +0545

    login & signup

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/',function (req, res) {
 const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
-app.use('/person',localAuthMiddleware,personRoutes);
+app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
 app.listen(PORT, ()=>{
