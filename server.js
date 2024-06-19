5d4a7ceb567ae69fac746f98c689c629ead91cc9
Sanjeet Shrestha <sanjeetshrestha5@gmail.com>
2024-06-19 14:03:50 +0545

    authentication

--- server.js
+++ server.js
@@ -70,21 +70,32 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+const logRequest = (req,res,next)=>{
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+  next();
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local',{session:false})
+
+app.get('/',function (req, res) {
   res.send('Welcome to our Hotel');
 })
+
 const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
-const MenuItem = require('./models/menuItem');
 
-app.use('/person',personRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Listening on port 3000');
 })
